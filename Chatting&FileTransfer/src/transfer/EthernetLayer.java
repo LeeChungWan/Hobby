29709ce45f541a1f360c698645018f4f3ef86c57
leechungwan <cnddhks846@naver.com>
2018-04-11 12:16:16 +0900

    added void resetHeaer()

--- Chatting&FileTransfer/src/transfer/EthernetLayer.java
+++ Chatting&FileTransfer/src/transfer/EthernetLayer.java
@@ -1,14 +1,28 @@
 package transfer;
 
 public class EthernetLayer extends BaseLayer{
-	final static int ETHERNET_HEADER_SIZE = 14;
-	final static int ETHERNET_MAX_DATA_SIZE = 1500;
-	final static int ETHERNET_TOTAL_MAX_SIZE = 1514;
+	final static int ETHERNET_HEADER_SIZE = 14;			// Ethernet header size.
+	final static int ETHERNET_MAX_DATA_SIZE = 1500;		// Ethernet 최대 데이터 크기
+	final static int ETHERNET_TOTAL_MAX_SIZE = 1514;	// Ethernet packet 최대 사이즈
 	
-	byte[] Ethernet_type;
-	byte[] Ethernet_src;
-	byte[] Ethernet_dst;
-	byte[] Ethernet_data;
+	byte[] Ethernet_type;	// 타입
+	byte[] Ethernet_src;	// 자기 MAC주소
+	byte[] Ethernet_dst;	// 목적지 MAC주소
+	byte[] Ethernet_data;	// 데이터 공간
+	
+	public EthernetLayer(String pName) {
+		super(pName);
+		resetHeader();
+	}
+	
+	void resetHeader(){
+		Ethernet_type = new byte[2];	// 0x0000의 사이즈로 생성. IPv4의 타입은 0x0800
+		Ethernet_type[0] = 8; 			// 0x08 설정 (앞부분)
+		Ethernet_type[1] = 0;			// 0x00 설정  (뒷부분)
+		Ethernet_src = new byte[6];
+		Ethernet_dst = new byte[6];
+		Ethernet_data = new byte[ETHERNET_MAX_DATA_SIZE];
+	}
 	
 	public byte[] getEthernet_src() {
 		return Ethernet_src;
@@ -26,9 +40,4 @@ public class EthernetLayer extends BaseLayer{
 		Ethernet_dst = ethernet_dst;
 	}
 
-	public EthernetLayer(String pName) {
-		super(pName);
-		
-	}
-	
 }
