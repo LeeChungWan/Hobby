1f1cb0dc3620549536da32fcf284bcd1c6ff55ca
leechungwan <cnddhks846@naver.com>
2018-04-17 01:22:41 +0900

    No1699 실패... 43에서 안됨. 다른 아이디어생각

---
+++ BeaKJoon/src/No1699.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class No1699 {
+	public static int pow_2(int num) {
+		return num * num;
+	}
+
+	public static int minimum(int num, int[] arr) {
+		int j = 0;
+		int value = num;
+		int count = 0;
+		int[] changedArr = null;
+		while (value != 0) {
+			// 이곳에서 i에 값을 유동적으로 바꾸어주어 성능 향상 시킬수 있을듯.
+			for (int i = j; i < arr.length; i++) {
+				if (value - arr[i] >= 0) {
+					value -= arr[i];
+					count++;
+					if (i != 0)
+						j = i;
+					break;
+				}
+			}
+		}
+
+		if (arr.length > 1) {
+			changedArr = new int[arr.length - 1];
+			for (int i = 0; i < changedArr.length; i++)
+				changedArr[i] = arr[i + 1];
+			return Math.min(count, minimum(num, changedArr));
+		} else
+			return count;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int sqrt = (int) Math.sqrt(num);
+		int[] arr = new int[sqrt];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = pow_2(sqrt);
+			sqrt--;
+		}
+		System.out.println(minimum(num, arr));
+		sc.close();
+	}
+
+}
