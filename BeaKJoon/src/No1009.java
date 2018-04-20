96db22e8f00cdf907dccfeefffc5daabc6401f90
leechungwan <cnddhks846@naver.com>
2018-04-20 22:49:06 +0900

    리펙토링 시간 1056MS -> 192MS 성능 향상

--- BeaKJoon/src/No1009.java
+++ BeaKJoon/src/No1009.java
@@ -7,6 +7,8 @@ public class No1009 {
 		int numOfCase = sc.nextInt();
 		int a, b;
 		int[] result = new int[numOfCase];
+		int[][] repeat = { { 1 }, { 2, 4, 8, 6 }, { 3, 9, 7, 1 }, { 4, 6 }, { 5 }, { 6 }, { 7, 9, 3, 1 },
+				{ 8, 4, 2, 6 }, { 9, 1 } };
 
 		for (int j = 0; j < numOfCase; j++) {
 			a = sc.nextInt();
@@ -15,10 +17,13 @@ public class No1009 {
 				result[j] = 10;
 			else {
 				int temp = a % 10;
-				for (int i = 1; i < b; i++) {
-					temp = (temp * a) % 10; // °ö¼ÀÀÇ 1ÀÇ ÀÚ¸®ÀÇ °ª.
+				int length = repeat[temp - 1].length;
+				int index = b % length;
+				if (index == 0) {
+					result[j] = repeat[temp-1][length-1];
+				} else {
+					result[j] = repeat[temp - 1][index - 1];
 				}
-				result[j] = temp;
 			}
 		}
 		for (int k = 0; k < numOfCase; k++)
