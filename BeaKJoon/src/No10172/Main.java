388e2953542e25b74fc4f19232c744732e66c19a
leechungwan <cnddhks846@naver.com>
2018-04-24 21:05:53 +0900

    No10172 개 성공

---
+++ BeaKJoon/src/No10172/Main.java
@@ -0,0 +1,24 @@
+package No10172;
+
+/*
+ * 문제 10172번 개
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.print("( "+ '0'+" )");
+		System.out.print('"');
+		System.out.print('"');
+		System.out.print('"');
+		System.out.println("\\");
+		System.out.print('|');
+		System.out.print('"');
+		System.out.print('^');
+		System.out.print('"');
+		System.out.println("`    |");
+		System.out.print("||_/=\\"+"\\__|");
+
+	}
+}
