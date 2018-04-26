6563c89dee414917af6d40c25ad9e50621f6025c
leechungwan <cnddhks846@naver.com>
2018-04-27 02:12:18 +0900

    No2448 별찍기 성공! / 공백으로 초기화 해줘야 맞다함...

--- BeaKJoon/src/No2448/Main.java
+++ BeaKJoon/src/No2448/Main.java
@@ -1,44 +1,40 @@
 package No2448;
 
 import java.util.Scanner;
-
+/*
+ * 문제 2448번 별찍기-11
+ * 
+ * 입력 : 첫째 줄에 N이 주어진다. N은 항상 3*2^k 수이다. (3, 6, 12, 24, 48, ...) (k<=10)
+ * 출력 : 첫째 줄부터 N번째 줄까지 별을 출력한다.
+ * @author 이충완
+ */
 public class Main {
-	static String[][] arr = new String[3072][3072];
+	static char[][] arr = null;
 
 	public static void makeStars(int N, int x, int y) {
 		if (N == 3) {
-			arr[x][y] = "*";
-			arr[x + 1][y - 1] = "*";
-			arr[x + 1][y + 1] = "*";
-			arr[x + 2][y - 2] = "*";
-			arr[x + 2][y - 1] = "*";
-			arr[x + 2][y] = "*";
-			arr[x + 2][y + 1] = "*";
-			arr[x + 2][y + 2] = "*";
+			arr[x][y] = '*';
+			arr[x + 1][y - 1] = arr[x + 1][y + 1] = arr[x + 2][y
+					- 2] = arr[x + 2][y - 1] = arr[x + 2][y] = arr[x + 2][y + 1] = arr[x + 2][y + 2] = '*';
 			return;
 		}
 		makeStars(N / 2, x, y);
 		makeStars(N / 2, x + N / 2, y - N / 2);
 		makeStars(N / 2, x + N / 2, y + N / 2);
+
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-
-		for (int i = 0; i < arr.length; i++) {
+		arr = new char[N][2 * N - 1];
+		for (int i = 0; i < arr.length; i++)
 			for (int j = 0; j < arr[i].length; j++)
-				arr[i][j] = " ";
-		}
-
+				arr[i][j] = ' ';
 		makeStars(N, 0, N - 1);
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < 2 * N - 1; j++) {
-				System.out.print(arr[i][j]);
-			}
-			System.out.println();
-		}
+		for (int i = 0; i < N; i++)
+			System.out.println(arr[i]);
 		sc.close();
 
 	}
