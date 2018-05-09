a25e51636c74af9c8c579ddcc8902de0d6a083f6
leechungwan <cnddhks846@naver.com>
2018-05-10 02:17:43 +0900

    카잉 어렵다 ㅠㅠㅠ

--- BeaKJoon/src/No6064/Main.java
+++ BeaKJoon/src/No6064/Main.java
@@ -14,26 +14,72 @@ public class Main {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 
-			int indexM = 0;
-			int indexN = 0;
-			int[][] arr = new int[M][N];
-			for (int j = 0; j < M * N; j++) {
-				if (indexM < M) {
-					if (indexN >= N)
-						indexN = 0;
-					arr[indexM++][indexN++] = j + 1;
-
-				} else {
-					indexM = 0;
-					if (indexN == N)
+			int sub = M - N;
+			int count = 0;
+			int compareValue = x;
+			boolean check = false;
+			// M > N 일 경우는 1:1 이라면 y의 값이 M-N만큼 증가.
+			if (x == y) {
+				result += x + "\n";
+				check = true;
+			} 
+			else if (M == 1 || N == 1) {
+				if (x == 1)
+					result += y + "\n";
+				else
+					result += x + "\n";
+				check = true;
+			}
+			else if (M == x && N == y) {
+				// M과 N의 최소 공배수가 result이다.
+				int temp1 = x, temp2=y;
+				if(x<y){
+					x = temp2;
+					y = temp1;
+				}
+				int r = 1;
+				// 유클리드 호제법을 이용한 최소공배수 구하기.
+				while(r>0){
+					r = x % y;
+					x = y;
+					y = r;
+				}
+				result += ((temp1*temp2)/x) + "\n";
+				check = true;
+			} 
+			else if (sub > 0) {
+				// M > N 일 경우는 1:1 이라면 y의 값이 M-N만큼 증가.
+				for (int j = 0; j < ((N / sub) + 1); j++) {
+					compareValue += sub;
+					count++;
+					if (compareValue > N) {
+						compareValue -= N;
+					}
+					if (compareValue == y) {
+						result += ((M * count) + x) + "\n";
+						check = true;
 						break;
-					arr[indexM++][indexN++] = j + 1;
+					}
+
+				}
+			} else if (sub < 0) {
+				// N < M 일 경우는 1:1 이라면 y의 값이 N-M만큼 감소.
+				for (int j = 0; j < (N / Math.abs(sub) + 1); j++) {
+					compareValue += sub;
+					count++;
+					if (compareValue < 0)
+						compareValue += N;
+
+					if (compareValue == y) {
+						result += ((M * count) + x) + "\n";
+						check = true;
+						break;
+					}
 				}
 			}
-			if (arr[x - 1][y - 1] != 0)
-				result += arr[x - 1][y - 1] + "\n";
-			else
+			if (!check)
 				result += "-1\n";
+
 		}
 		System.out.println(result);
 		sc.close();
