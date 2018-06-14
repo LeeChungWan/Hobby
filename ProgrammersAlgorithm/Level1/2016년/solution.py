24991f52aff86350606acc4cb7cc0c1cd9c3adc5
leechungwan <cnddhks846@naver.com>
2018-06-14 23:34:17 +0900

    2016년

---
+++ ProgrammersAlgorithm/Level1/2016년/solution.py
@@ -0,0 +1,14 @@
+"""
+2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요.
+요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT 입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 TUE를 반환하세요.
+"""
+def solution(a, b):
+    month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    sum = 0
+    for i in range(0,a-1):
+        sum += month[i]
+    sum += b
+    index = (sum%7) - 1
+    answer = day[index]
+    return answer
